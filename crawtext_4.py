c380df9b641a6dc189b41d1150bc95945ad994e1
c24b <4barbes@gmail.com>
2014-03-13 18:03:28 +0100

    cleaning output, handling report error detail

--- crawtext_4.py
+++ crawtext_4.py
@@ -63,7 +63,10 @@ class Page(object):
 		
 	def bad_status(self):
 		self.status = False
-		self.error = {"url":self.url, "error_code": self.req.status_code, "type": self.error_type, "status": False}
+		try:
+			self.error = {"url":self.url, "error_code": self.req.status_code, "type": self.error_type, "status": False}
+		except AttributeError:
+			self.error = {"url":self.url, "error_code": 404, "type": self.error_type, "status": False}
 		return
 
 	def pre_check(self):
@@ -95,24 +98,30 @@ class Page(object):
 	
 	def request(self):
 		print "connect to", self.url
-		requests.adapters.DEFAULT_RETRIES = 2
-		user_agents = [u'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1', u'Mozilla/5.0 (Windows NT 6.1; rv:15.0) Gecko/20120716 Firefox/15.0a2', u'Mozilla/5.0 (compatible; MSIE 10.6; Windows NT 6.1; Trident/5.0; InfoPath.2; SLCC1; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; .NET CLR 2.0.50727) 3gpp-gba UNTRUSTED/1.0', u'Opera/9.80 (Windows NT 6.1; U; es-ES) Presto/2.9.181 Version/12.00']
-		headers = {'User-Agent': choice(user_agents),}
-		proxies = {"https":"77.120.126.35:3128", "https":'88.165.134.24:3128', }
-		self.req = requests.get((self.url), headers = headers,allow_redirects=True, proxies=proxies, timeout=5)
-		
-		if self.pre_check() and self.check():
-			try:
-				self.src = self.req.text
-					
-			except Exception, e:
-				self.error_type = str(e)
+		try:
+			requests.adapters.DEFAULT_RETRIES = 2
+			user_agents = [u'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1', u'Mozilla/5.0 (Windows NT 6.1; rv:15.0) Gecko/20120716 Firefox/15.0a2', u'Mozilla/5.0 (compatible; MSIE 10.6; Windows NT 6.1; Trident/5.0; InfoPath.2; SLCC1; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; .NET CLR 2.0.50727) 3gpp-gba UNTRUSTED/1.0', u'Opera/9.80 (Windows NT 6.1; U; es-ES) Presto/2.9.181 Version/12.00']
+			headers = {'User-Agent': choice(user_agents),}
+			proxies = {"https":"77.120.126.35:3128", "https":'88.165.134.24:3128', }
+			self.req = requests.get((self.url), headers = headers,allow_redirects=True, proxies=proxies, timeout=5)
+			print self.req.status_code
+			if self.pre_check() and self.check():
+				try:
+					self.src = self.req.text
+						
+				except Exception, e:
+					self.error_type = "Request answer was not understood %s" %e
+					self.bad_status()
+				
+			else:
+				self.error_type = "Not relevant"
 				self.bad_status()
 			
-		else:
-			self.error_type = "Not relevant"
+		
+		except requests.exceptions.MissingSchema:
+			self.error_type = "Incorrect url %s" %self.url
 			self.bad_status()
-						
+		return self
 	def extract(self):
 		try:
 			self.soup = bs(self.src)
@@ -120,13 +129,20 @@ class Page(object):
 			article = g.extract(raw_html=self.src)
 			self.title = article.title
 			self.text = article.cleaned_text
-			self.description = article.meta_description
-
+			self.description = bs(article.meta_description).text
 		except Exception, e:
 			self.error_type = str(e)
 			self.bad_status()
 		return self
-		
+	
+	def get(self):
+		self.result = {}
+		_inherited = ["status", "description", "text","outlinks","backlinks","url","title"]
+		for name in self.__dict__:
+			if name in _inherited:
+				self.result.update({name :self.__dict__[name]})
+		return self.result
+				
 	def clean_url(self, url):
 		self.netloc = 'http://' + urlparse(self.url)[1]
 		if url not in [ '#', None, '\n', '' ] and 'javascript' not in url:
@@ -138,6 +154,7 @@ class Page(object):
 			elif urlparse(url)[0] == '':
 				url = 'http:' + url
 			return url
+	
 	def next_step(self):
 		from multiprocessing import Pool
 		if self.status is True:
@@ -165,12 +182,12 @@ class Page(object):
 			 	
 		
 	def store(self):
-		return self.__dict__	
-		
+		results = self.__dict__	
+		return self.__dict__
 	
 		
 if __name__ == '__main__':
-	liste = ["http://www.tourismebretagne.com/informations-pratiques/infos-environnement/algues-vertes","http://www.developpement-durable.gouv.fr/Que-sont-les-algues-vertes-Comment.html"]
+	liste = ["http://www.tourismebretagne.com/informations-pratiques/infos-environnement/algues-vertes","http://www.developpement-durable.gouv.fr/Que-sont-les-algues-vertes-Comment.html", "test.com"]
 	query= "algues vertes OR algue verte"
 	db = Database("test_crawltext_4")
 	db.create_tables()
@@ -180,21 +197,9 @@ if __name__ == '__main__':
 		p.request()
 		p.extract()
 		p.next_step()
+		result = p.get()
 		
-		result = p.store()
 		if p.status is False:
 			print p.error
-			#db.report.insert(p.error)
-			liste.pop(n)
 		else:
-			print result
-			db.results.insert(result)
-			liste.append(result['outlinks'])
-			liste.pop(n)
-			#print result['outlinks']
-		
-		#~ 
-		#~ print p.outlinks
-		#~ p.do_page()
-		#~ 
-	
+			print result['title']
