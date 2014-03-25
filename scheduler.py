49d3cd8e09e9cc36f7c59571c006c0c664d5b927
C2_4b <4barbes@gmail.com>
2014-03-25 10:51:00 +0100

    problems with doc vs implementation, cron not exiting properly

--- scheduler.py
+++ scheduler.py
@@ -3,9 +3,11 @@
 
 from apscheduler.scheduler import Scheduler
 from apscheduler.jobstores.mongodb_store import MongoDBJobStore
+from apscheduler.scheduler import SchedulerEvent, JobStoreEvent
 #from apscheduler.jobstores.mongodb import MongoDBJobStore
 import time, sys  
 import pymongo
+
 
 def schedule(docopt_args, job):
 	if docopt_args['--repeat'] or docopt_args['start']:
@@ -14,20 +16,23 @@ def schedule(docopt_args, job):
 		#store = MongoDBJobStore(database=db, collection=db.jobs, connection=mongo)
 		#store = MongoDBJobStore(connection=mongo, collection=collection)  
 		sched = Scheduler(daemonic = False)
+		sched.start()
 		#sched.add_jobstore(store, docopt_args['<project>'])
-		sched.add_cron_job(job, day_of_week='mon', hour=5, minute=30)
-		#sched.start()
-		sched.print_jobs()
+		print sched.get_jobs()
+		sched.add_job(job, 'cron', {'day_of_week': 'mon-fri', 'hour': 5, 'minute': 30})
+		
+		print sched.get_jobs()
 		print "Job %s is actually running every Monday @5:30" %docopt_args['<project>'] 
-		return sys.exit()
+		
 
 def unschedule(docopt_args):
-	mongo = pymongo.Connection(host='127.0.0.1', port=27017)
-	collection = mongo.jobs
-	store = MongoDBJobStore(connection=collection)  
+	#mongo = pymongo.Connection(host='127.0.0.1', port=27017)
+	#collection = mongo.jobs
+	#store = MongoDBJobStore(connection=collection)  
 	sched = Scheduler(daemonic = False)
 	#sched.shutdown(wait=True, shutdown_threadpool=True, close_jobstores=True)
 	try:
+		print sched.get_jobs()
 		sched.unschedule_job(store)
 		print sched.get_jobs()
 	except KeyError:
