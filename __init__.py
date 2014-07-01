fec2a0e0f58b5ad7f6807d7392e3b86a2b5bad14
C2_4b <4barbes@gmail.com>
2014-07-02 00:27:17 +0200

    Renettoyage

--- __init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-""" CLI Simple Crawler """
-__version__="1.0.0"
-
-from crawtext import crawtext
--- cfg.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env/python
-# -*- coding: utf-8 -*-
-
-# Add here your GMAIL Credentials (needed for report)
-username = '' #without @gmail.com
-password = ''
